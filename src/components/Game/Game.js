8fbf75bf36c052b3d31fce2f383af65a392a0338
Kamlesh Thakur <thakurkamlesh10@gmail.com>
2023-03-14 13:35:21 +0545

    Task 1 and 2 complete

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,13 +3,27 @@ import React from 'react'
 import { sample } from '../../utils'
 import { WORDS } from '../../data'
 
+import GuessInput from '../GuessInput'
+import GuessResult from '../GuessResult/GuessResult'
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS)
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer })
 
 function Game() {
-  return <>Put a game here!</>
+  const [guesses, setGuesses] = React.useState([])
+
+  function handleGuess(guess) {
+    const nextGuesses = [...guesses, guess]
+    setGuesses(nextGuesses)
+  }
+  return (
+    <>
+      <GuessResult guesses={guesses} />
+      <GuessInput addGuessResults={handleGuess} />
+    </>
+  )
 }
 
 export default Game
