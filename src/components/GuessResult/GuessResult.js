630fca149ebc52aec771b32c2eda65f3389f7e1f
Kamlesh Thakur <thakurkamlesh10@gmail.com>
2023-03-14 14:57:17 +0545

    Task 4 complete

--- src/components/GuessResult/GuessResult.js
+++ src/components/GuessResult/GuessResult.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { range } from '../../utils'
 import Guess from '../Guess'
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
-function GuessResult({ guesses }) {
+function GuessResult({ guesses, answer }) {
   return (
     <div className='guess-results'>
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} guess={guesses[num]} />
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   )
